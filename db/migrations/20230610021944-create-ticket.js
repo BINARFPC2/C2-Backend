43626756c8471843d8d263677130e1ef773650e1
taofikarianto <taofikarianto@gmail.com>
2023-06-10 10:06:59 +0700

    add seeders ticket

--- db/migrations/20230610021944-create-ticket.js
+++ db/migrations/20230610021944-create-ticket.js
@@ -1,55 +1,55 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Tickets', {
+    await queryInterface.createTable("Tickets", {
       id: {
+        defaultValue: Sequelize.UUIDV4,
         allowNull: false,
-        autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.UUID,
       },
       city_from: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       city_to: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       airport_from: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       airport_to: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       dateDeparture: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       dateArrival: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       type_seat: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       total_passenger: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       price: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       available: {
-        type: Sequelize.BOOLEAN
+        type: Sequelize.BOOLEAN,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Tickets');
-  }
+    await queryInterface.dropTable("Tickets");
+  },
 };
