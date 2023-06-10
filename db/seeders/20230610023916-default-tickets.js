28fbb9a6a30aa9e163ffffb3edc884f9ad372166
taofikarianto <taofikarianto@gmail.com>
2023-06-10 18:01:16 +0700

    update seeders ticket

--- db/seeders/20230610023916-default-tickets.js
+++ db/seeders/20230610023916-default-tickets.js
@@ -19,6 +19,21 @@ module.exports = {
         createdAt: new Date(),
         updatedAt: new Date(),
       },
+      {
+        id: uuid(),
+        city_from: "Yogyakarta (JOG)",
+        city_to: "Jakarta (JKTA)",
+        airlines: "Garuda",
+        airport_from: "Yogyakarta International Airport",
+        airport_to: "Soekarno-Hatta International",
+        dateDeparture: "2023-06-03T07:55:00+00:00",
+        dateArrival: "2023-06-03T09:15:00+00:00",
+        type_seat: "Business",
+        price: 1384985,
+        available: true,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
     ]);
   },
 
