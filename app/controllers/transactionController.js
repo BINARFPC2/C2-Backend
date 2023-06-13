0f3fe99750926114d9294c8324c46573fc182eda
taofikarianto <taofikarianto@gmail.com>
2023-06-14 03:31:23 +0700

    update

--- app/controllers/transactionController.js
+++ app/controllers/transactionController.js
@@ -5,7 +5,9 @@ const Transaction = require("../models");
 async function createTransaction(req, res) {
   try {
     // Mengambil data tiket dari tabel "Ticket"
-    const tickets = await Ticket.findAll();
+    const tickets = () => {
+      return Ticket.findAll();
+    };
 
     // Memasukkan data tiket ke dalam tabel "Transaction"
     const transactions = await Promise.all(
