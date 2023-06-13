3be2aad12afa0e8717267c93b2e2cc7cbc76732f
taofikarianto <taofikarianto@gmail.com>
2023-06-14 01:02:53 +0700

    update models

--- app/models/user.js
+++ app/models/user.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      this.hasMany(models.transaction, { foreignKey: "usersId" });
+      this.hasMany(models.transactions, { foreignKey: "usersId" });
     }
   }
 
