5194f4218f47d9c6063bb6bdd0536b3ea2e3ddc4
taofikarianto <taofikarianto@gmail.com>
2023-06-14 03:57:55 +0700

    update

--- app/controllers/transactionController.js
+++ app/controllers/transactionController.js
@@ -2,31 +2,25 @@
 const Ticket = require("../models");
 const Transaction = require("../models");
 
-async function createTransaction(req, res) {
-  try {
-    // Mengambil data tiket dari tabel "Ticket"
-    const tickets = await Ticket.findAll();
-
-    // Memasukkan data tiket ke dalam tabel "Transaction"
-    const transactions = await Promise.all(
-      tickets.map((ticket) => {
-        return Transaction.create({
-          ticketId: ticket.id,
-          // quantity: ticket.quantity,
-        });
-      })
-    );
-
-    res.json({
-      message: 'Data telah dimasukkan ke tabel "Transaction"',
-      transactions,
-    });
-  } catch (error) {
-    console.error("Terjadi kesalahan:", error);
-    res.status(500).json({ error: "Terjadi kesalahan" });
-  }
-}
-
 module.exports = {
-  createTransaction,
+  async createTransaction(req, res) {
+    try {
+      const dataTicket = await Ticket.findOne({
+        where: { booking_code: req.body.booking_code },
+      });
+
+      const ticketId = await Transaction.findOne({
+        where: { ticketsId: dataTicket.id },
+      });
+
+      if (ticketId) {
+        await Transaction.create({
+          ticketsId: dataTicket.id,
+        });
+      }
+      res.status(200).json({
+        status: "Ok",
+      });
+    } catch (error) {}
+  },
 };
