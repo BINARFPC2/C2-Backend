6c121cb52dff1dca63e1cfa41e9e097f52d84e07
taofikarianto <taofikarianto@gmail.com>
2023-06-13 23:51:14 +0700

    add feature/transaction

---
+++ db/migrations/20230613164416-create-transaction.js
@@ -0,0 +1,44 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Transactions", {
+      id: {
+        defaultValue: Sequelize.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        unique: true,
+      },
+      usersId: {
+        type: Sequelize.UUID,
+      },
+      ticketsId: {
+        type: Sequelize.UUID,
+      },
+      checkoutsId: {
+        type: Sequelize.UUID,
+      },
+      amounts: {
+        type: Sequelize.INTEGER,
+      },
+      date: {
+        type: Sequelize.DATE,
+      },
+      status: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Transactions");
+  },
+};
