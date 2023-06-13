44f0963f1b721fbca940f79c51ac2df11dbd9773
taofikarianto <taofikarianto@gmail.com>
2023-06-14 00:59:43 +0700

    update models

--- app/models/transaction.js
+++ app/models/transaction.js
@@ -8,7 +8,9 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      this.belongsTo(models.user, { foreignKey: "usersId" });
+      this.belongsTo(models.ticket, { foreignKey: "ticketsId" });
+      this.belongsTo(models.checkout, { foreignKey: "checkoutsId" });
     }
   }
   Transaction.init(
