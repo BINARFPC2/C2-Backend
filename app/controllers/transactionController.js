4fa7f3069ebd55f7f137d3beb98b29e77bc900d0
taofikarianto <taofikarianto@gmail.com>
2023-06-14 14:18:05 +0700

    update transaction controller

--- app/controllers/transactionController.js
+++ app/controllers/transactionController.js
@@ -54,16 +54,16 @@ module.exports = {
   async createTransaction(req, res) {
     try {
       // Mengambil data user dari model User berdasarkan ID user
-      const iduser = await user.findByPk(req.body.usersId);
+      const iduser = await user.findByPk(req.body.userId);
 
       // Mengambil data tiket dari model Ticket berdasarkan ID tiket
-      const ticket = await Ticket.findByPk(req.body.ticketsId);
+      const ticket = await Ticket.findByPk(req.body.ticketId);
 
       // Membuat transaksi baru dengan data yang diambil
       const transaction = await Transaction.create({
         id: uuid(),
-        usersId: iduser.id,
-        ticketsId: ticket.id,
+        usersId: iduser,
+        ticketsId: ticket,
         // quantity: req.body.quantity,
         // Setel nilai-nilai kolom lainnya yang diperlukan
       });
