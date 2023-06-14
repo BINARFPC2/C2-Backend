c15060fac218a4677ed38ef35cd4d163464d4788
taofikarianto <taofikarianto@gmail.com>
2023-06-14 12:09:44 +0700

    update api-docs

--- app/controllers/transactionController.js
+++ app/controllers/transactionController.js
@@ -29,7 +29,7 @@ module.exports = {
     } catch (error) {
       res.status(500).json({
         status: "Error",
-        message: error.message,
+        message: "booking_code is already in use",
       });
     }
   },
