90aa00421336947203ffba7ba3e18a0ec5ba711f
taofikarianto <taofikarianto@gmail.com>
2023-06-16 02:36:10 +0700

    add reset password

--- app/controllers/forgetPasswordController.js
+++ app/controllers/forgetPasswordController.js
@@ -94,7 +94,7 @@ module.exports = {
                 margin: 4px 2px;color: white;
                 padding: 16px 32px;
                 transition-duration: 0.4s;" 
-                href='https://c2-backend.up.railway.app//api/v1/reset-password/${token}' target="_blank" rel="reset">Reset Password</a>
+                href='https://c2-backend.up.railway.app/api/v1/reset-password/${token}' target="_blank" rel="reset">Reset Password</a>
               </button>
             <center>
                     `,
