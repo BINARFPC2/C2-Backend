0e2a5bdbf2c1c4841a74c4171c405f0a8e2d3191
taofikarianto <taofikarianto@gmail.com>
2023-06-19 01:03:14 +0700

    update ticket

--- app/controllers/ticketController.js
+++ app/controllers/ticketController.js
@@ -132,9 +132,15 @@ module.exports = {
 
     try {
       const ticket = await Ticket.findByPk(idTicket);
-      const totalPrice = ticket.calculateTotalPrice(); // Hitung total price menggunakan calculateTotalPrice()
 
-      updateData.total_price = totalPrice; // Atur nilai total price
+      // Hitung total price baru
+      const totalPrice = ticket.price * total_passenger;
+
+      // Update total_price di data tiket
+      ticket.total_price = totalPrice;
+
+      // Simpan perubahan data tiket ke database
+      await ticket.save();
 
       await Ticket.update(updateData, {
         where: { id: idTicket },
