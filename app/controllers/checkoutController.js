da26979017dc3e9101a051c6fadfefcac479c032
taofikarianto <taofikarianto@gmail.com>
2023-06-21 05:11:53 +0700

    update

--- app/controllers/checkoutController.js
+++ app/controllers/checkoutController.js
@@ -58,7 +58,7 @@ module.exports = {
       // create passengers for the ticket
       for (const passengerData of passengers) {
         await Passenger.create({
-          checkoutsId: checkout.ticketsId,
+          checkoutsId: ticketsId,
           name: passengerData.name,
           email: passengerData.email,
           phone: passengerData.phone,
