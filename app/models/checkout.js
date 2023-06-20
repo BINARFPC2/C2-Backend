b5055c627f26173858307bc98e17929be9e7f20f
taofikarianto <taofikarianto@gmail.com>
2023-06-21 04:59:14 +0700

    update

--- app/models/checkout.js
+++ app/models/checkout.js
@@ -10,6 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       this.hasMany(models.Transaction, { foreignKey: "checkoutsId" });
+      this.hasMany(models.Passenger, { foreignKey: "ticketsId" });
     }
   }
   Checkout.init(
