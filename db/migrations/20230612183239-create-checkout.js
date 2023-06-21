a09395e9e5b0ca2b38f8dc63db61de6e7bbf13cb
taofikarianto <taofikarianto@gmail.com>
2023-06-22 01:34:40 +0700

    update

--- db/migrations/20230612183239-create-checkout.js
+++ db/migrations/20230612183239-create-checkout.js
@@ -19,6 +19,7 @@ module.exports = {
         type: Sequelize.UUID,
         defaultValue: Sequelize.UUIDV4,
         allowNull: false,
+        primaryKey: true,
       },
       total_passenger: {
         type: Sequelize.INTEGER,
