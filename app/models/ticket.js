34da37339a0466f2cbf70ba093d73cc0b2633bdb
taofikarianto <taofikarianto@gmail.com>
2023-06-22 00:02:56 +0700

    update

--- app/models/ticket.js
+++ app/models/ticket.js
@@ -9,6 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       this.hasMany(models.Transaction, { foreignKey: "ticketsId" });
+      this.belongsTo(models.Checkout, { foreignKey: "checkoutsId" });
     }
     // calculateTotalPrice() {
     //   return this.price * this.total_passenger;
