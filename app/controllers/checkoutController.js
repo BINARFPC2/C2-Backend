9ab54f944a2fa06bfcd2ce07f1438555b744f3e5
taofikarianto <taofikarianto@gmail.com>
2023-06-21 18:11:36 +0700

    update checkout

--- app/controllers/checkoutController.js
+++ app/controllers/checkoutController.js
@@ -151,7 +151,7 @@ module.exports = {
             {
               model: Ticket,
               where: {
-                id: sequelize.col("checkout.ticketsId"),
+                id: sequelize.col("Checkout.ticketsId"),
               },
             },
           ],
