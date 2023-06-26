309b910f508aea01843686f4bc4a8c4501fd6e11
taofikarianto <taofikarianto@gmail.com>
2023-06-26 14:31:41 +0700

    update payment

--- app/models/payment.js
+++ app/models/payment.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      this.hasMany(models.Checkout, { foreignKey: "usersId", as: "usersId" }); // Perbarui foreign key
+      // this.hasMany(models.Checkout, { foreignKey: "usersId", as: "usersId" }); // Perbarui foreign key
     }
   }
   Payment.init(
