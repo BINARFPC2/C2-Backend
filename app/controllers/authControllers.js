a2126a7dbb35f2d27afe62dbb5202dfca1441292
taofikarianto <taofikarianto@gmail.com>
2023-07-05 01:55:08 +0700

    update otp

--- app/controllers/authControllers.js
+++ app/controllers/authControllers.js
@@ -174,7 +174,7 @@ module.exports = {
 
       // Generate otp
       const otp = generateOTP();
-      const otpExpirationValidity = 2; // Menentukan validitas kedaluwarsa OTP dalam menit
+      const otpExpirationValidity = 1; // Menentukan validitas kedaluwarsa OTP dalam menit
       const otpExpiration = new Date();
       otpExpiration.setMinutes(
         otpExpiration.getMinutes() + otpExpirationValidity
